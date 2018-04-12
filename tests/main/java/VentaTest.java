d2d8fdc1779958e4583a41fa2ca9aaa354313589
Alan Szpigiel <alanszp@gmail.com>
2018-04-12 20:39:02 -0300

    add failing test

--- tests/main/java/VentaTest.java
+++ tests/main/java/VentaTest.java
@@ -10,4 +10,13 @@ public class VentaTest {
 
         venta.cobrar();
     }
+
+    @Test(expected = RuntimeException.class)
+    public void probarQueLaVentaPuedeFallar() {
+        Cliente cliente = new Cliente("alan", "szpigiel", "123123", "798789", "4567");
+        MedioDePago pagoVisa = new PagoVisa();
+        Venta venta = new Venta(cliente, 100, pagoVisa);
+
+        venta.cobrar();
+    }
 }
