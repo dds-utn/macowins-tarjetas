44e25bf2cb48d16fa9c557ee316c1130139d9467
Alan Szpigiel <alanszp@gmail.com>
2018-04-12 20:36:44 -0300

    add working tests

--- tests/main/java/VentaTest.java
+++ tests/main/java/VentaTest.java
@@ -5,7 +5,16 @@ public class VentaTest {
     @Test
     public void probarQueLaVentaPuedeCobrarSatisfactoriamente() {
         Cliente cliente = new Cliente("alan", "szpigiel", "123123", "798789", "4567");
-        MedioDePago pagoVisa = new PagoVisa();
+        MedioDePago pagoVisa = new PagoSiempreAprueba();
+        Venta venta = new Venta(cliente, 100, pagoVisa);
+
+        venta.cobrar();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void probarQueLaVentaPuedeFallar() {
+        Cliente cliente = new Cliente("alan", "szpigiel", "123123", "798789", "4567");
+        MedioDePago pagoVisa = new PagoSiempreFalla();
         Venta venta = new Venta(cliente, 100, pagoVisa);
 
         venta.cobrar();
